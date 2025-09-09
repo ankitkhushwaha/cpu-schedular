b2eeaae5a1a5a030ea55f2290ef70b669b22bb36
Ankit khushwaha <ankitkhushwaha.dev@gmail.com>
2025-09-09 15:46:13 +0530

    change process ds

--- src/process.h
+++ src/process.h
@@ -1,11 +1,20 @@
 #ifndef PROCESS_H
 #define PROCESS_H
 
+#include <stdbool.h>
+
 extern int TOTAL_PROCESS;
 
-typedef struct {
+typedef struct
+{
     int pid;
-}process_t;
+    bool cpu_use;
+    bool io_use;
+    // 0 = terminated, < 0 = sleep, > 0 = running 
+    int status;
+    int cpu_time;
+    int io_time;
+} process_t;
 
 process_t *create_process();
 #endif
